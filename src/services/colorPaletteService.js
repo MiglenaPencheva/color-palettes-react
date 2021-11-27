469a03dfd599ce562b6532ab347c684bfd99b0b9
Miglena Pencheva <miglena_pencheva@abv.bg>
2021-11-27 18:44:21 +0200

    colorPaletteService updated

--- src/services/colorPaletteService.js
+++ src/services/colorPaletteService.js
@@ -1,22 +1,30 @@
-const baseUrl = '';
+const baseUrl = 'http://localhost:5500';
 
 export const getAll = async () => {
-    let response = await fetch(`${baseUrl}/color-palettes`);
-    let result = await response.json();
+    try {
+        let response = await fetch(`${baseUrl}/color-palettes/`);
+        let result = await response.json();
 
-    let colorPalettes = Object.values(result);
-    return colorPalettes;
+        let colorPalettes = Object.values(result);
+        return colorPalettes;
+    } catch (error) {
+        return { msg: error };
+    }
 };
 
 export const create = async (data) => {
-    let response = await fetch(`${baseUrl}/color-palettes`, {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(data)
-    });
+    try {
+        let response = await fetch(`${baseUrl}/color-palettes/`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+        let result = await response.json();
+        return result;
+    } catch (error) {
+        return { msg: error };
+    }
 
-    let result = await response.json();
-    return result;
 };
 
 export const getOne = async (id) => {
