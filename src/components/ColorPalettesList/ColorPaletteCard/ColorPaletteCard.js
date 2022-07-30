4852c1cb97196137bf6c31c354d1a9a873aed574
Miglena Pencheva <miglena_pencheva@abv.bg>
2022-07-30 11:46:33 +0300

    Image fixed

--- src/components/ColorPalettesList/ColorPaletteCard/ColorPaletteCard.js
+++ src/components/ColorPalettesList/ColorPaletteCard/ColorPaletteCard.js
@@ -7,7 +7,7 @@ const ColorPaletteCard = ({
         <li className="otherPet">
             <h3>{colorPalette.title}</h3>
             <p>Category: {colorPalette.category}</p>
-            <p className="img"><img src={colorPalette.imageUrl} alt="color palette" /></p>
+            <p className="img"><img src={colorPalette.imageFile} alt="color palette" /></p>
             <Link className="button" to={`/details/${colorPalette._id}`}>Details</Link>
         </li>
     );
