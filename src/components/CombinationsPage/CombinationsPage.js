546c8f09da76f27b10cd4be64b8d772df6dda452
Miglena Pencheva <miglena_pencheva@abv.bg>
2022-08-24 21:41:38 +0300

    combinations updates

--- src/components/CombinationsPage/CombinationsPage.js
+++ src/components/CombinationsPage/CombinationsPage.js
@@ -13,6 +13,24 @@ const Combinations = () => {
 
     // const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
+    const onWhiteChange = (e) => {
+        const whiteLayer = document.getElementById('whiteLayer');
+        const w = Number(e.target.value) / 100;
+        whiteLayer.style['background-color'] = `rgba(255,255,255,${w})`;
+        setWhiteValue(e.target.value);
+    };
+    const onBlackChange = (e) => {
+        const wheel = document.getElementById('rybImage');
+        const bl = 100 - (Number(e.target.value));
+        wheel.style.filter = `brightness(${bl}%)`;
+        setBlackValue(e.target.value);
+    };
+    const onGreyChange = (e) => {
+        const wheel = document.getElementById('rybImage');
+        wheel.style.filter = `grayscale(${e.target.value}%)`;
+        setGreyValue(e.target.value);
+    };
+
     return (
         <section className="ryb">
             <h2 className="ryb__header">Color combinations</h2>
@@ -36,7 +54,8 @@ const Combinations = () => {
 
                 <section className="ryb__actions--wheel">
                     <h6>RYB color wheel</h6>
-                    <img id="rybImage" src="/images/wheel.png" alt="ryb wheel" width="300" height="300" />
+                    <img id="rybImage" src="/images/ryb.png" alt="ryb wheel" width="300" height="300" />
+                    <div className="white-layer" id="whiteLayer"></div>
                     <span>Pick a color from the wheel</span>
                 </section>
 
@@ -45,32 +64,51 @@ const Combinations = () => {
                     <div className="ryb__actions--settings-sliders">
                         <p>Set the quantity of white, black or grey to change the lightness, darkness or saturation of the pure color</p>
 
-                        <label htmlFor="whiteRange">
+                        <label htmlFor="whiteRange" className="slider-label">
                             White
                             <input type="range" className="slider" id="whiteRange" min="0" max="100"
-                                onChange={(e) => setWhiteValue(e.target.value)} value={whiteValue} />
+                                onChange={onWhiteChange} value={whiteValue} />
                             <span>{whiteValue}%</span>
                         </label>
                         <div>Add white to lighten the color.
                             <br /> Tints are pastel, pale, cool versions of the hue.</div>
 
-                        <label htmlFor="blackRange">
+                        <label htmlFor="blackRange" className="slider-label">
                             Black
+                            {/* <span>{brightnessValue}%</span> */}
                             <input type="range" className="slider" id="blackRange" min="0" max="100"
-                                onChange={(e) => setBlackValue(e.target.value)} value={blackValue} />
+                                onChange={onBlackChange} value={blackValue} />
                             <span>{blackValue}%</span>
                         </label>
                         <div>Add black to darken the color.
                             <br />Shades are deep, warm and more intensive.</div>
 
-                        <label htmlFor="greyRange">
+                        <label htmlFor="greyRange" className="slider-label">
                             Grey
                             <input type="range" className="slider" id="greyRange" min="0" max="100"
-                                onChange={(e) => setGreyValue(e.target.value)} value={greyValue} />
+                                onChange={onGreyChange} value={greyValue} />
                             <span>{greyValue}%</span>
                         </label>
                         <div>Add grey to desaturate the color.
                             <br />Tones are muted and more colorless.</div>
+
+                        
+
+                        <section className="select-container">
+                            <label htmlFor="scheme" className="select-container__label">Color scheme</label>
+                            <select id="scheme" name="scheme"
+                                // onChange={e => setScheme(e.target.value)}
+                                className="select">
+                                <option value="Choose scheme" className="default-scheme">Choose scheme</option>
+                                <option value="complementary">Complementary</option>
+                                <option value="splitComplementary">Split-complementary</option>
+                                <option value="monochromatic">Monochromatic</option>
+                                <option value="analogous">Analogous</option>
+                                <option value="triadic">Triadic</option>
+                                <option value="tetradic">Tetradic</option>
+                                <option value="square">Square</option>
+                            </select>
+                        </section>
 
                     </div>
                 </section>
