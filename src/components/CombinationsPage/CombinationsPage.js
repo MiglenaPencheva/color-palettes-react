2c030f4e24fb8f070d93e854da07ef4b4b093881
Miglena Pencheva <miglena_pencheva@abv.bg>
2022-08-27 18:54:29 +0300

    Combinations wheel, sliders and colors updated

--- src/components/CombinationsPage/CombinationsPage.js
+++ src/components/CombinationsPage/CombinationsPage.js
@@ -1,42 +1,98 @@
 import { useState, useEffect } from 'react';
+import { getColorName } from './combinationsHelpers';
 
 const Combinations = () => {
 
+    // const [src, setSrc] = useState('');
+    const [data, setData] = useState([]);
+    const [pickedColor, setPickedColor] = useState('#608d9e');
     const [whiteValue, setWhiteValue] = useState(0);
     const [blackValue, setBlackValue] = useState(0);
     const [greyValue, setGreyValue] = useState(0);
 
-    // const image = document.getElementById('rybImage');
-    // const canvas = document.getElementById('rybCanvas');
-    // const ctx = canvas.getContext('2d');
-    // ctx.drawImage(image, 10, 10);
+    const makeRybCanvas = (e) => {
+        const canvas = document.getElementById('rybCanvas');
+        const ryb = document.getElementById('rybImage');
 
-    // const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(ryb, 0, 0, 300, 300);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        setData(imageData.data);
+    };
+
+    const getPixel = (e) => {
+        resetSettings();
+
+        let { offsetX, offsetY } = e.nativeEvent;
+        let pixel = e.target.width * offsetY + offsetX;
+        let arrayPos = pixel * 4;
+        const c = {
+            red: data[arrayPos],
+            green: data[arrayPos + 1],
+            blue: data[arrayPos + 2],
+            alpha: data[arrayPos + 3],
+        };
+        const picked = `rgb(${c.red}, ${c.green}, ${c.blue})`;
+
+        const colorName = getColorName[picked];
+        console.log(colorName);
+
+        setPickedColor(picked);
+    };
 
     const onWhiteChange = (e) => {
         const whiteLayer = document.getElementById('whiteLayer');
         const w = Number(e.target.value) / 100;
         whiteLayer.style['background-color'] = `rgba(255,255,255,${w})`;
+        whiteLayer.style.display = 'block';
+        resetBlack();
+        resetGrey();
         setWhiteValue(e.target.value);
     };
     const onBlackChange = (e) => {
-        const wheel = document.getElementById('rybImage');
+        const canvas = document.getElementById('rybCanvas');
         const bl = 100 - (Number(e.target.value));
-        wheel.style.filter = `brightness(${bl}%)`;
+        canvas.style.filter = `brightness(${bl}%)`;
+        resetWhite();
+        setGreyValue(0);
         setBlackValue(e.target.value);
     };
     const onGreyChange = (e) => {
-        const wheel = document.getElementById('rybImage');
-        wheel.style.filter = `grayscale(${e.target.value}%)`;
+        const canvas = document.getElementById('rybCanvas');
+        canvas.style.filter = `grayscale(${e.target.value}%)`;
+        resetWhite();
+        setBlackValue(0);
         setGreyValue(e.target.value);
     };
 
+    const resetWhite = (e) => {
+        const whiteLayer = document.getElementById('whiteLayer');
+        whiteLayer.style.display = 'none';
+        whiteLayer.style['background-color'] = 'rgba(255,255,255,0)';
+        setWhiteValue(0);
+    };
+    const resetBlack = (e) => {
+        const canvas = document.getElementById('rybCanvas');
+        canvas.style.filter = 'brightness(0)';
+        setBlackValue(0);
+    };
+    const resetGrey = (e) => {
+        const canvas = document.getElementById('rybCanvas');
+        canvas.style.filter = 'grayscale(0)';
+        setGreyValue(0);
+    };
+    const resetSettings = (e) => {
+        resetWhite();
+        resetBlack();
+        resetGrey();
+    };
+
     return (
-        <section className="ryb">
+        <section className="ryb" >
             <h2 className="ryb__header">Color combinations</h2>
-            <h6> Find the colors that go well together.</h6>
-            <h6> Combine vibes in nice and accurate color schemes.</h6>
-            <h6> Be the designer of your colorful life.</h6>
+            <h6> Find the colors that go well together.
+                Combine vibes in nice and accurate color schemes.</h6>
+            <h6 className="diffHeading"> Be the designer of your colorful life.</h6>
 
             <p> The traditional color theory is based on subtractive primary colors and the RYB color model.</p>
             <p> Red, Yellow and Blue are the<strong className="strong"> primary colors</strong>.
@@ -54,15 +110,35 @@ const Combinations = () => {
 
                 <section className="ryb__actions--wheel">
                     <h6>RYB color wheel</h6>
-                    <img id="rybImage" src="/images/ryb.png" alt="ryb wheel" width="300" height="300" />
-                    <div className="white-layer" id="whiteLayer"></div>
+                    <div className="white-layer" onClick={resetWhite} id="whiteLayer"></div>
+                    <canvas id="rybCanvas" onClick={getPixel} width="300" height="300">
+                        <img id="rybImage" src='/images/ryb.png' alt="ryb" onLoad={makeRybCanvas} />
+                    </canvas>
                     <span>Pick a color from the wheel</span>
                 </section>
 
                 <section className="ryb__actions--settings">
-                    <h6>Settings of colors</h6>
-                    <div className="ryb__actions--settings-sliders">
-                        <p>Set the quantity of white, black or grey to change the lightness, darkness or saturation of the pure color</p>
+                    <h6>Settings</h6>
+
+                    <section className="select-container">
+                        <label htmlFor="scheme" className="select-container__label">Color scheme</label>
+                        <select id="scheme" name="scheme"
+                            // onChange={e => setScheme(e.target.value)}
+                            className="select">
+                            <option value="Choose scheme" className="default-scheme">Choose scheme</option>
+                            <option value="complementary">Complementary</option>
+                            <option value="splitComplementary">Split-complementary</option>
+                            <option value="monochromatic">Monochromatic</option>
+                            <option value="analogous">Analogous</option>
+                            <option value="triadic">Triadic</option>
+                            <option value="tetradic">Tetradic</option>
+                            <option value="square">Square</option>
+                        </select>
+                    </section>
+
+                    <section className="ryb__actions--settings-sliders">
+                        <p>Set the quantity of white, black or grey to change
+                            the lightness, darkness or saturation of pure colors</p>
 
                         <label htmlFor="whiteRange" className="slider-label">
                             White
@@ -91,28 +167,12 @@ const Combinations = () => {
                         </label>
                         <div>Add grey to desaturate the color.
                             <br />Tones are muted and more colorless.</div>
-
-                        
-
-                        <section className="select-container">
-                            <label htmlFor="scheme" className="select-container__label">Color scheme</label>
-                            <select id="scheme" name="scheme"
-                                // onChange={e => setScheme(e.target.value)}
-                                className="select">
-                                <option value="Choose scheme" className="default-scheme">Choose scheme</option>
-                                <option value="complementary">Complementary</option>
-                                <option value="splitComplementary">Split-complementary</option>
-                                <option value="monochromatic">Monochromatic</option>
-                                <option value="analogous">Analogous</option>
-                                <option value="triadic">Triadic</option>
-                                <option value="tetradic">Tetradic</option>
-                                <option value="square">Square</option>
-                            </select>
-                        </section>
-
-                    </div>
+                    </section>
+                    
+                    <button onClick={resetSettings} className="button">reset</button>
                 </section>
             </section>
+
 
         </section>
     );
---
+++ src/components/CombinationsPage/combinationsHelpers.js
@@ -0,0 +1,15 @@
+export const getColorName = {
+    'rgb(254, 254, 51)': 'yellow',
+    'rgb(252, 204, 26)': 'yellow-orange',
+    'rgb(251, 153, 2)': 'orange',
+    'rgb(252, 96, 10)': 'red-orange',
+    'rgb(254, 39, 18)': 'red',
+    'rgb(194, 20, 96)': 'red-purple',
+    'rgb(134, 1, 175)': 'purple',
+    'rgb(68, 36, 214)': 'blue-purple',
+    'rgb(2, 71, 254)': 'blue',
+    'rgb(52, 124, 152)': 'blue-green',
+    'rgb(102, 176, 50)': 'green',
+    'rgb(178, 215, 50)': 'yellow-green',
+};
+
