5fd143258d974665c7974d3b9202a74f27abb6eb
Miglena Pencheva <miglena_pencheva@abv.bg>
2022-09-25 22:46:30 +0300

    SchemeDetails component added

--- src/components/CombinationsPage/combinationsHelpers.js
+++ src/components/CombinationsPage/combinationsHelpers.js
@@ -395,45 +395,7 @@ export const getInfoCombinations = {
     'analogous3': 'Analogous scheme uses colors that are next to each other on the color wheel. It is easy to create and gives a pleasant and elegant appearance. One dominant color and the others as supporting or accents make this blend harmonious and calming. The lack of contrast keeps it less vibrant. This kind of combination occurs in nature and colors never clash one another. Neighboring hues fits better if they are either in the warm or the cold gamma.',
     'analogous5': 'Analogous scheme uses colors that are next to each other on the color wheel. It is easy to create and gives a pleasant and elegant appearance. One dominant color and the others as supporting or accents make this blend harmonious and calming. The lack of contrast keeps it less vibrant. This kind of combination occurs in nature and colors never clash one another. Neighboring hues fits better if they are either in the warm or the cold gamma.', 
     'triadic': 'Triadic scheme is made up of three hues equally spaced around the wheel. One base color is balanced with two evenly spaced colors - accents. This combination offers high visual contrast and a harmony at the same time. The vibrance remains even if pale or unsaturated variations of hues are used. The triadic scheme is easier to accomplish and provides dynamics and richness of the colors.',
-    'tetradic1': 'this is tetradic1 scheme',
-    'tetradic2': 'this is tetradic2 scheme',
-    'square': 'this is square scheme',
+    'tetradic1': 'Tetradic scheme is also called a double complementary scheme. The four colors arranged in two complementary pairs make it difficult to harmonize. One of the colors should be dominant, the others may be pastels or unsaturated variants of the hues. The pairs form a rectangle or a square if the chosen colors stands on equal space one from another. The tetradic scheme is the richest of all combinations and give the opportunity to use many color variations.',
+    'tetradic2': 'Tetradic scheme is also called a double complementary scheme. The four colors arranged in two complementary pairs make it difficult to harmonize. One of the colors should be dominant, the others may be pastels or unsaturated variants of the hues. The pairs form a rectangle or a square if the chosen colors stands on equal space one from another. The tetradic scheme is the richest of all combinations and give the opportunity to use many color variations.',
+    'square': 'Tetradic scheme is also called a double complementary scheme. The four colors arranged in two complementary pairs make it difficult to harmonize. One of the colors should be dominant, the others may be pastels or unsaturated variants of the hues. The pairs form a rectangle or a square if the selected colors stands on equal space one from another. The tetradic scheme is the richest of all combinations and give the opportunity to choose many color variations.',
 };
-
-// The monochromatic scheme uses a single base color and various tints, 
-// tones and shades of the same hue, that are derived by adding white, grey or black. 
-// It is easy to create and easy to apply and perceive. 
-// This color scheme gives a soft and pleasant feeling. 
-// The lack of contrast makes more subtle and peaceful vision. 
-// Dynamics can be achieved combining dark shades and light tints or even black and white. 
-// Using one base color with its variations gives bold and dramatic effect, as well as stylish and elegant look';
-
-// Analogous scheme uses colors that are next to each other on the color wheel.
-// It is easy to create and gives a pleasant and elegant appearance.
-// One dominant color and the others as supporting or accents make this blend harmonious and calming.
-// The lack of contrast keeps it less vibrant. 
-// This kind of combination occurs in nature and colors never clash one another.
-// Neighboring hues fits better if they are either in the warm or the cold gamma.
-
-// Complementary scheme takes colors from opposite side of the color wheel.
-// This is the most contrasting of all color schemes. It attracts the most attention.
-// One of the hues is the dominant color of the pair. 
-// The other one enhances or emphasizes the primary one and is used for accents.
-// Both warm and cold colors take part in this most dynamic harmony.
-// The design looks warm or cold according to the chosen dominant color.
-// Attractive and hard for balancing, complementary scheme gives sharp contrast, brighter and prominent vision.
-
-// Split-complementary scheme is a variation of complementary scheme.
-// Takes a base color and the two colors on both sides of the opposite one on the color wheel.
-// Has the same sharp visual contrast and still gives the balance between warm or cold color temperatures.
-// Cold base color should stand opposite of two variations of warm hues, for example.
-// 3-color harmony offers less pressure, less tension and is not so vibrant.
-// It is hard to harmonize and difficult for balancing.
-// But still gives the best contrast, beautiful nuances and a pleasant feeling.
-
-// Triadic scheme is made up of three hues equally spaced around the wheel.
-// One base color is balanced with two evenly spaced colors - accents.
-// This combination offers high visual contrast and a harmony at the same time.
-// The vibrance remains, even if pale or unsaturated variations of hues are used.
-// The triadic scheme is easier to accomplish and provides dynamics and richness of the colors.
-
