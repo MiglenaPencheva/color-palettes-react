f4a59f7fc7d5976c8bd28c26cdfd46906d952600
Miglena Pencheva <miglena_pencheva@abv.bg>
2022-10-09 00:06:32 +0300

    Clear local storage

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,21 @@
 import { useNavigate } from 'react-router-dom';
+import useLocalStorage from '../../hooks/useLocalStorage';
+
+const initialHexState = {
+    hex: '#94c7db',
+    rgb: 'rgb(148, 199, 219)',
+};
 
 const Logo = () => {
     const navigate = useNavigate();
 
+    const [hexToExplore, setHexToExplore] = useLocalStorage('hex', initialHexState);
+
+
     const showMenu = (e) => {
         e.target.style.transform = 'rotate(360deg)';
         e.target.style['transition-duration'] = '1.5s';
+        setHexToExplore(initialHexState);
         navigate('/');
     };
 
