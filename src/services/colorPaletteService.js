99959f085a13b53e62289ba6490371741786d98d
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-03-11 21:50:23 +0200

    formData instead of data

--- src/services/colorPaletteService.js
+++ src/services/colorPaletteService.js
@@ -86,10 +86,11 @@ export const update = async (colorPaletteId, data, token) => {
     let response = await fetch(`${baseUrl}/color-palettes/${colorPaletteId}`, {
         method: 'PUT',
         headers: {
-            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/json',
             'x-authorization': token
         },
-        body: JSON.stringify(data)
+        body: data
+        // body: JSON.stringify(data)
     });
 
     let result = await response.json();
