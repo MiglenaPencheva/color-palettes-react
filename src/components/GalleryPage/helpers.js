11948985ba1f33812c49958d11627d4b8b0b4183
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-04-20 20:55:02 +0300

    more initial palettes added

--- src/components/GalleryPage/helpers.js
+++ src/components/GalleryPage/helpers.js
@@ -15,7 +15,7 @@ export const initialPalettes = [
         title: 'Colorful stones and pebbles on rocky beach, wet from the rain.',
         category: 'others',
         src: '/images/initial/3.jpg',
-        likes: 24 
+        likes: 24
     },
     {
         title: 'The violet blossom of Rhododendron ponticum in Strandzha mountains.',
@@ -35,64 +35,74 @@ export const initialPalettes = [
         src: '/images/initial/6.jpg',
         likes: 18
     },
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/7.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/8.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/9.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/10.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/11.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/12.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/13.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/14.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/15.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/16.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/17.jpg',
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: /images/initial/18.jpg',
-    //     likes: 
-    // }
+    {
+        title: 'Flat pebbles in pyramid stack on the sand on sunny beach and blurred sea water background',
+        category: 'sea',
+        src: '/images/initial/7.jpg',
+        likes: 15
+    },
+    {
+        title: 'Autumn colors -green, yellow and orange leaves on the branches of a tree.',
+        category: 'plants',
+        src: '/images/initial/8.jpg',
+        likes: 12
+    }, 
+    {
+        title: 'Pastel violet and creamy yellow blossom of freesia flower',
+        category: 'plants',
+        src: '/images/initial/9.jpg',
+        likes: 10
+    }, 
+    {
+        title: 'Autumn forest with brown tree stems, green, yellow and orange leaves',
+        category: 'landscapes',
+        src: '/images/initial/10.jpg',
+        likes: 9
+    }, 
+    {
+        title: 'Yarn texture of beige, brown, green, turquoise and blue strains.',
+        category: 'others',
+        src: '/images/initial/11.jpg',
+        likes: 8
+    }, {
+        title: 'Beautiful clouds in the sky, variety of shades and tones. ',
+        category: 'sky',
+        src: '/images/initial/12.jpg',
+        likes: 7
+    }, 
+    {
+        title: 'Green spring field of blooming purple violet Consolida regalis (Forking Larkspur, Rocket lark)',
+        category: 'landscapes',
+        src: '/images/initial/13.jpg',
+        likes: 6
+    }, {
+        title: 'Yarn knitting texture background, pastel beige, creamy brown and rosy pink strains',
+        category: 'others',
+        src: '/images/initial/14.jpg',
+        likes: 5
+    }, 
+    {
+        title: 'Dark brown bark of tree in coniferous forest and green ivy leaves on the beige ground',
+        category: 'landscapes',
+        src: '/images/initial/15.jpg',
+        likes: 4
+    }, 
+    {
+        title: 'Turquoise sea water with beige brown wet sand on sunny beach',
+        category: 'sea',
+        src: '/images/initial/16.jpg',
+        likes: 3
+    }, 
+    {
+        title: 'The colors of the autumn, yellow, orange and brown leaves by the wooden bench in the park',
+        category: 'landscapes',
+        src: '/images/initial/17.jpg',
+        likes: 2
+    }, 
+    {
+        title: 'Green wheat field landscape and blue mountains on rainy cloudy sky',
+        category: 'landscapes',
+        src: '/images/initial/18.jpg',
+        likes: 1
+    }
 ];
