d04e82d4c5d10a42cb87d5b7bcfb37e1277fcfdb
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-04-20 19:50:19 +0300

    more initials added

--- src/components/GalleryPage/helpers.js
+++ src/components/GalleryPage/helpers.js
@@ -10,85 +10,89 @@ export const initialPalettes = [
         category: 'plants',
         src: '/images/initial/2.jpg',
         likes: 30
-    }
+    },
+    {
+        title: 'Colorful stones and pebbles on rocky beach, wet from the rain.',
+        category: 'others',
+        src: '/images/initial/3.jpg',
+        likes: 24 
+    },
+    {
+        title: 'The violet blossom of Rhododendron ponticum in Strandzha mountains.',
+        category: 'plants',
+        src: '/images/initial/4.jpg',
+        likes: 22
+    },
+    {
+        title: 'Seascape of turquoise and dark blue sea water with brown beige sand.',
+        category: 'sea',
+        src: '/images/initial/5.jpg',
+        likes: 21
+    },
+    {
+        title: 'Gray, beige and brown colors of the sparrow.',
+        category: 'animals',
+        src: '/images/initial/6.jpg',
+        likes: 18
+    },
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/7.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/8.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/9.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/10.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/11.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/12.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/13.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/14.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/15.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/16.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
+    //     src: /images/initial/17.jpg',
     //     likes: 
     // },{
     //     title: '',
     //     category: ,
-    //     src: ,
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: ,
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: ,
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: ,
-    //     likes: 
-    // },{
-    //     title: '',
-    //     category: ,
-    //     src: ,
+    //     src: /images/initial/18.jpg',
     //     likes: 
     // }
 ];
