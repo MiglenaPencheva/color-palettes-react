7160bbe06603d784a868975d1037baa765b3a97b
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-04-20 18:57:00 +0300

    img fixed

--- src/components/GalleryPage/helpers.js
+++ src/components/GalleryPage/helpers.js
@@ -2,13 +2,13 @@ export const initialPalettes = [
     {
         title: 'Brown frappe drink on beige sandy beach and blue sea background.',
         category: 'foodAndDrinks',
-        src: '/images/initial/1',
+        src: '/images/initial/1.jpg',
         likes: 44
     },
     {
         title: 'Kazanlak Damascena rose on blurred green blue mountains background in the valley of roses',
         category: 'plants',
-        src: '/images/initial/2',
+        src: '/images/initial/2.jpg',
         likes: 30
     }
     //     title: '',
