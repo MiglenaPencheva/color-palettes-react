3909b8c30e964e0995b8db3f0120381997ae3327
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-11-22 19:08:44 +0200

    Language fixed

--- src/components/Language/Language.js
+++ src/components/Language/Language.js
@@ -3,23 +3,17 @@ import { useLanguageContext } from '../../contexts/LanguageContext';
 const Language = () => {
     const { language, toggleLanguage } = useLanguageContext();
 
-    const activeEn = `
-        <span id="activeEn">
-            <span className="activeLanguage">en</span>
-            <span className="notActiveLanguage">bg</span>
-        </span>`;
-
-    const activeBg = `
-        <span id="activeBg">
-            <span className="notActiveLanguage">en</span>
-            <span className="activeLanguage">bg</span>
-        </span>`;
-
     return (
         <section id="toggleLanguage" onClick={toggleLanguage}>
             { language === 'en' 
-                ? activeEn
-                : activeBg
+                ? <span id="activeEn">
+                    <span className="activeLanguage">en</span>
+                    <span className="notActiveLanguage">bg</span>
+                </span>
+                : <span id="activeBg">
+                    <span className="notActiveLanguage">en</span>
+                    <span className="activeLanguage">bg</span>
+                </span>
             }
         </section>
     );
