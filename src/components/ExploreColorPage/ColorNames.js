a6b7ccd05b5b399ae47c634cfb6a1eebacadea49
Miglena Pencheva <miglena_pencheva@abv.bg>
2024-01-17 12:45:05 +0200

    translation optimization

--- src/components/ExploreColorPage/ColorNames.js
+++ src/components/ExploreColorPage/ColorNames.js
@@ -11,26 +11,20 @@ const ColorNames = () => {
     return (
         <section className="explore__color-names">
 
+            <h5 className="explore__info">{language.lang === 'en' ? 'Color names' : 'Имената на цветовете'}</h5>
+
             {language.lang === 'en' ? (
-                <>
-                    <h5 className="explore__info">Color names</h5>
-
-                    <article>
-                        There are 140 standard color names that are supported by all modern browsers and CSS.
-                        <br />Color samples, names and hex codes are listed below in an alphabetical order.
-                        Click on a chosen one to view details and explore it in the color values <Link target="_blank" to="/color-explore/convertor"><i>convertor.</i></Link>
-                    </article>
-                </>
+                <article>
+                    There are 140 standard color names that are supported by all modern browsers and CSS.
+                    <br />Color samples, names and hex codes are listed below in an alphabetical order.
+                    Click on a chosen one to view details and explore it in the color values <Link target="_blank" to="/color-explore/convertor"><i>convertor.</i></Link>
+                </article>
             ) : (
-                <>
-                    <h5 className="explore__info">Имената на цветовете</h5>
-
-                    <article>
-                        Съвременните браузъри поддържат 140 стандартни имена.
-                        <br />Цветовете с имената им и техните Хекс стойности (шеснайсетични кодове) са изброени по-надолу по азбучен ред.
-                        Кликни върху цвят, за да го разгледаш подробно в <Link target="_blank" to="/color-explore/convertor"><i>конвертора</i></Link> на цветните стойности. 
-                    </article>
-                </>
+                <article>
+                    Съвременните браузъри поддържат 140 стандартни имена.
+                    <br />Цветовете с имената им и техните Хекс стойности (шеснайсетични кодове) са изброени по-надолу по азбучен ред.
+                    Кликни върху цвят, за да го разгледаш подробно в <Link target="_blank" to="/color-explore/convertor"><i>конвертора</i></Link> на цветните стойности.
+                </article>
             )}
 
             <ColorNamesList list={list} />
--- src/components/ExploreColorPage/ExploreGroups.js
+++ src/components/ExploreColorPage/ExploreGroups.js
@@ -23,73 +23,36 @@ const ExploreGroups = () => {
 
             <h5 className="explore__info">{language.lang === 'en' ? 'Colors by groups' : 'Цветовете по групи'}</h5>
 
-            {language.lang === 'en' ? (
-                <>
-                    <h5 className="explore__info">Colors by groups</h5>
+            <article>{language.lang === 'en'
+                ? 'There are 140 standard color names that are supported by all modern browsers and CSS. Color samples, names and hex codes are listed below, separated in color groups.'
+                : 'Съвременните браузъри поддържат 140 стандартни имена. Цветовете с имената им и техните Хекс стойности (шеснайсетични кодове) са изброени по-надолу по групи.'}
+            </article>
 
-                    <article>There are 140 standard color names that are supported by all modern browsers and CSS.
-                        Color samples, names and hex codes are listed below, separated in color groups.
-                    </article>
+            <section className="explore__groups" id="exploreGroups">
+                <li onClick={() => setYellow(!yellow)} style={{ 'backgroundColor': '#ffffba' }}>{language.lang === 'en' ? 'Yellow colors' : 'Жълти цветове'}</li>
+                {yellow && <ColorNamesList list={getGroup['yellow']} />}
+                <li onClick={() => setRed(!red)} style={{ 'backgroundColor': '#ffb6ae' }}>{language.lang === 'en' ? 'Red colors' : 'Червени цветове'}</li>
+                {red && <ColorNamesList list={getGroup['red']} />}
+                <li onClick={() => setBlue(!blue)} style={{ 'backgroundColor': '#a9c0ff' }}>{language.lang === 'en' ? 'Blue colors' : 'Сини цветове'}</li>
+                {blue && <ColorNamesList list={getGroup['blue']} />}
+                <li onClick={() => setOrange(!orange)} style={{ 'backgroundColor': '#fedca9' }}>{language.lang === 'en' ? 'Orange colors' : 'Оранжеви цветове'}</li>
+                {orange && <ColorNamesList list={getGroup['orange']} />}
+                <li onClick={() => setPurple(!purple)} style={{ 'backgroundColor': '#d6a9e4' }}>{language.lang === 'en' ? 'Purple colors' : 'Лилави цветове'}</li>
+                {purple && <ColorNamesList list={getGroup['purple']} />}
+                <li onClick={() => setGreen(!green)} style={{ 'backgroundColor': '#cbe4b9' }}>{language.lang === 'en' ? 'Green colors' : 'Зелени цветове'}</li>
+                {green && <ColorNamesList list={getGroup['green']} />}
+                <li onClick={() => setCyan(!cyan)} style={{ 'backgroundColor': '#c1ffff' }}>{language.lang === 'en' ? 'Cyan colors' : 'Цианови цветове'}</li>
+                {cyan && <ColorNamesList list={getGroup['cyan']} />}
+                <li onClick={() => setBrown(!brown)} style={{ 'backgroundColor': '#d8bdbd' }}>{language.lang === 'en' ? 'Brown colors' : 'Оранжеви цветове'}</li>
+                {brown && <ColorNamesList list={getGroup['brown']} />}
+                <li onClick={() => setPink(!pink)} style={{ 'backgroundColor': '#ffdfe5' }}>{language.lang === 'en' ? 'Pink colors' : 'Розови цветове'}</li>
+                {pink && <ColorNamesList list={getGroup['pink']} />}
+                <li onClick={() => setWhite(!white)} style={{ 'backgroundColor': '#fffaf0' }}>{language.lang === 'en' ? 'White colors' : 'Бели цветове'}</li>
+                {white && <ColorNamesList list={getGroup['white']} />}
+                <li onClick={() => setGrey(!grey)} style={{ 'backgroundColor': '#dee2e6' }}>{language.lang === 'en' ? 'Grey colors' : 'Сиви цветове'}</li>
+                {grey && <ColorNamesList list={getGroup['grey']} />}
+            </section>
 
-                    <section className="explore__groups" id="exploreGroups">
-                        <li onClick={() => setYellow(!yellow)} style={{ 'backgroundColor': '#ffffba' }}>Yellow colors</li>
-                        {yellow && <ColorNamesList list={getGroup['yellow']} />}
-                        <li onClick={() => setRed(!red)} style={{ 'backgroundColor': '#ffb6ae' }}>Red colors</li>
-                        {red && <ColorNamesList list={getGroup['red']} />}
-                        <li onClick={() => setBlue(!blue)} style={{ 'backgroundColor': '#a9c0ff' }}>Blue colors</li>
-                        {blue && <ColorNamesList list={getGroup['blue']} />}
-                        <li onClick={() => setOrange(!orange)} style={{ 'backgroundColor': '#fedca9' }}>Orange colors</li>
-                        {orange && <ColorNamesList list={getGroup['orange']} />}
-                        <li onClick={() => setPurple(!purple)} style={{ 'backgroundColor': '#d6a9e4' }}>Purple colors</li>
-                        {purple && <ColorNamesList list={getGroup['purple']} />}
-                        <li onClick={() => setGreen(!green)} style={{ 'backgroundColor': '#cbe4b9' }}>Green colors</li>
-                        {green && <ColorNamesList list={getGroup['green']} />}
-                        <li onClick={() => setCyan(!cyan)} style={{ 'backgroundColor': '#c1ffff' }}>Cyan colors</li>
-                        {cyan && <ColorNamesList list={getGroup['cyan']} />}
-                        <li onClick={() => setBrown(!brown)} style={{ 'backgroundColor': '#d8bdbd' }}>Brown colors</li>
-                        {brown && <ColorNamesList list={getGroup['brown']} />}
-                        <li onClick={() => setPink(!pink)} style={{ 'backgroundColor': '#ffdfe5' }}>Pink colors</li>
-                        {pink && <ColorNamesList list={getGroup['pink']} />}
-                        <li onClick={() => setWhite(!white)} style={{ 'backgroundColor': '#fffaf0' }}>White colors</li>
-                        {white && <ColorNamesList list={getGroup['white']} />}
-                        <li onClick={() => setGrey(!grey)} style={{ 'backgroundColor': '#dee2e6' }}>Grey colors</li>
-                        {grey && <ColorNamesList list={getGroup['grey']} />}
-                    </section>
-                </>
-            ) : (
-                <>
-                    <h5 className="explore__info">Цветовете по групи</h5>
-
-                    <article>Съвременните браузъри поддържат 140 стандартни имена.
-                        Цветовете с имената им и техните Хекс стойности (шеснайсетични кодове) са изброени по-надолу по групи.
-                    </article>
-
-                    <section className="explore__groups" id="exploreGroups">
-                        <li onClick={() => setYellow(!yellow)} style={{ 'backgroundColor': '#ffffba' }}>Жълти цветове</li>
-                        {yellow && <ColorNamesList list={getGroup['yellow']} />}
-                        <li onClick={() => setRed(!red)} style={{ 'backgroundColor': '#ffb6ae' }}>Червени цветове</li>
-                        {red && <ColorNamesList list={getGroup['red']} />}
-                        <li onClick={() => setBlue(!blue)} style={{ 'backgroundColor': '#a9c0ff' }}>Сини цветове</li>
-                        {blue && <ColorNamesList list={getGroup['blue']} />}
-                        <li onClick={() => setOrange(!orange)} style={{ 'backgroundColor': '#fedca9' }}>Оранжеви цветове</li>
-                        {orange && <ColorNamesList list={getGroup['orange']} />}
-                        <li onClick={() => setPurple(!purple)} style={{ 'backgroundColor': '#d6a9e4' }}>Лилави цветове</li>
-                        {purple && <ColorNamesList list={getGroup['purple']} />}
-                        <li onClick={() => setGreen(!green)} style={{ 'backgroundColor': '#cbe4b9' }}>Зелени цветове</li>
-                        {green && <ColorNamesList list={getGroup['green']} />}
-                        <li onClick={() => setCyan(!cyan)} style={{ 'backgroundColor': '#c1ffff' }}>Цианови цветове</li>
-                        {cyan && <ColorNamesList list={getGroup['cyan']} />}
-                        <li onClick={() => setBrown(!brown)} style={{ 'backgroundColor': '#d8bdbd' }}>Оранжеви цветове</li>
-                        {brown && <ColorNamesList list={getGroup['brown']} />}
-                        <li onClick={() => setPink(!pink)} style={{ 'backgroundColor': '#ffdfe5' }}>Розови цветове</li>
-                        {pink && <ColorNamesList list={getGroup['pink']} />}
-                        <li onClick={() => setWhite(!white)} style={{ 'backgroundColor': '#fffaf0' }}>Бели цветове</li>
-                        {white && <ColorNamesList list={getGroup['white']} />}
-                        <li onClick={() => setGrey(!grey)} style={{ 'backgroundColor': '#dee2e6' }}>Сиви цветове</li>
-                        {grey && <ColorNamesList list={getGroup['grey']} />}
-                    </section>
-                </>
-            )}
         </section>
     );
 };
