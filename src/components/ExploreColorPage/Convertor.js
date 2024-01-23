6342d15ec9e5becade32c6f15a5c0978c82f220b
Miglena Pencheva <miglena_pencheva@abv.bg>
2024-01-23 23:26:31 +0200

    Convertor translated

--- src/components/ExploreColorPage/Convertor.js
+++ src/components/ExploreColorPage/Convertor.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useLanguageContext } from '../../contexts/LanguageContext';
 import useLocalStorage from '../../hooks/useLocalStorage';
 import * as helpers from '../ExploreColorPage/exploreHelpers';
 import { getPixel } from '../../helpers/getPixel';
@@ -12,6 +13,7 @@ const initialHexState = {
 };
 
 const Convertor = () => {
+    const { language } = useLanguageContext();
 
     const [selectedColor, setSelectedColor] = useState('#94c7db');
     const [currentColor, setCurrentColor] = useState('#94c7db');
@@ -226,52 +228,103 @@ const Convertor = () => {
 
     return (
         <section className="convertor">
-            <section className="convertor__input-section">
-                <form id="color-form" className="convertor__input--form" onSubmit={submitColorHandler}>
-                    <h5 className="explore__info">Type color value</h5>
-                    <p className="convertor__input--info">Enter name, rgb, hex or hsl value</p>
-                    <section className="convertor__input--container">
-                        <input type="text" name="colorValue" id="colorValue" className="convertor__input--input" placeholder="color value" />
-                        <input className="button convertor__input--submit" type="submit" value="ok" />
+            {language.lang === 'en' ? (
+                <>
+                    <section className="convertor__input-section">
+                        <form id="color-form" className="convertor__input--form" onSubmit={submitColorHandler}>
+                            <h5 className="explore__info">Type color value</h5>
+                            <p className="convertor__input--info">Enter name, rgb, hex or hsl value</p>
+                            <section className="convertor__input--container">
+                                <input type="text" name="colorValue" id="colorValue" className="convertor__input--input" placeholder="color value" />
+                                <input className="button convertor__input--submit" type="submit" value="ok" />
+                            </section>
+                        </form>
+                        <section className="convertor__pick-color">
+                            <h5 className="explore__info">or pick a color</h5>
+                            <canvas id="rgbCanvas" width="270" height="40" onClick={selectColor}></canvas>
+                            <div>Quantity of white, black or grey changes the lightness, darkness or saturation of pure colors.</div>
+                            <canvas id="whiteCanvas" width="270" height="40" onClick={modifyColor}></canvas>
+                            <div>White lightens the color. Tints are <Link to="/combinations/pastels" target="_blank"><i>pastel</i></Link>, pale, cool versions of the hue.</div>
+                            <canvas id="blackCanvas" width="270" height="40" onClick={modifyColor}></canvas>
+                            <div>Black darkens the color. Shades are deep, warm and more intensive.</div>
+                            <canvas id="greyCanvas" width="270" height="40" onClick={modifyColor}></canvas>
+                            <div>Grey unsaturates the color. Tones are muted and more colorless.</div>
+                        </section>
                     </section>
-                </form>
-                <section className="convertor__pick-color">
-                    <h5 className="explore__info">or pick a color</h5>
-                    <canvas id="rgbCanvas" width="270" height="40" onClick={selectColor}></canvas>
-                    <div>Quantity of white, black or grey changes the lightness, darkness or saturation of pure colors</div>
-                    <canvas id="whiteCanvas" width="270" height="40" onClick={modifyColor}></canvas>
-                    <div>White lightens the color. Tints are <Link  to="/combinations/pastels" target="_blank"><i>pastel</i></Link>, pale, cool versions of the hue.</div>
-                    <canvas id="blackCanvas" width="270" height="40" onClick={modifyColor}></canvas>
-                    <div>Black darkens the color. Shades are deep, warm and more intensive.</div>
-                    <canvas id="greyCanvas" width="270" height="40" onClick={modifyColor}></canvas>
-                    <div>Grey unsaturates the color. Tones are muted and more colorless.</div>
-                </section>
-            </section>
 
-            <section className="explore__preview">
-                <h5 className="explore__info">Color preview</h5>
-                <div id="previewWitheText">White text</div>
-                <div id="previewTextOnWithe">White background</div>
-                <div id="previewBlackText">Black text</div>
-                <div id="previewTextOnBlack">Black background</div>
-            </section>
+                    <section className="explore__preview">
+                        <h5 className="explore__info">Color preview</h5>
+                        <div id="previewWitheText">White text</div>
+                        <div id="previewTextOnWithe">White background</div>
+                        <div id="previewBlackText">Black text</div>
+                        <div id="previewTextOnBlack">Black background</div>
+                    </section>
 
-            <section className="explore__values">
-                <h5 className="explore__info">Color values</h5>
-                <li>{name ? name : 'no name'}</li>
-                <span>Browsers support 140 <Link target="_blank" to="/color-explore/color-names"><i>color names.</i></Link></span>
-                <li>{rgb}</li>
-                <span>RGB value indicates how much of red, green and blue is included.
-                    <br /> Each component of the triplet can vary from 0 to 255.</span>
-                <li>#{hex}</li>
-                <span>A hex triplet is a six-digit, three-byte hexadecimal number, specifying the intensity of red, green and blue.</span>
-                <li>hsl({hsl})</li>
-                <span>HSL color value is specified with hue, saturation and lightness parameters.
-                    {/* <br />Saturation and lightness are a percentage value from 0% to 100%. */}
-                </span>
-                <li>cmyk({cmyk})</li>
-                <span>CMYK refers to the four ink plates used in color printing: cyan, magenta, yellow, and key (black).</span>
-            </section>
+                    <section className="explore__values">
+                        <h5 className="explore__info">Color values</h5>
+                        <li>{name ? name : 'no name'}</li>
+                        <span>Browsers support 140 <Link target="_blank" to="/color-explore/color-names"><i>color names.</i></Link></span>
+                        <li>{rgb}</li>
+                        <span>RGB value indicates how much of red, green and blue is included.
+                            <br /> Each component of the triplet can vary from 0 to 255.</span>
+                        <li>#{hex}</li>
+                        <span>A hex triplet is a six-digit, three-byte hexadecimal number, specifying the intensity of red, green and blue.</span>
+                        <li>hsl({hsl})</li>
+                        <span>HSL color value is specified with hue, saturation and lightness parameters.
+                            {/* <br />Saturation and lightness are a percentage value from 0% to 100%. */}
+                        </span>
+                        <li>cmyk({cmyk})</li>
+                        <span>CMYK refers to the four ink plates used in color printing: cyan, magenta, yellow, and key (black).</span>
+                    </section>
+                </>
+            ) : (
+                <>
+                    <section className="convertor__input-section">
+                        <form id="color-form" className="convertor__input--form" onSubmit={submitColorHandler}>
+                            <h5 className="explore__info">Въведи стойност</h5>
+                            <p className="convertor__input--info">Въведи име, rgb, hex или hsl стойност</p>
+                            <section className="convertor__input--container">
+                                <input type="text" name="colorValue" id="colorValue" className="convertor__input--input" placeholder="стойност" />
+                                <input className="button convertor__input--submit" type="submit" value="въведи" />
+                            </section>
+                        </form>
+                        <section className="convertor__pick-color">
+                            <h5 className="explore__info">или избери цвят</h5>
+                            <canvas id="rgbCanvas" width="270" height="40" onClick={selectColor}></canvas>
+                            <div>Количеството бяло, черно или сиво променя яркостта или наситеността на чистите цветове.</div>
+                            <canvas id="whiteCanvas" width="270" height="40" onClick={modifyColor}></canvas>
+                            <div>Бялото изсветлява цвета. Светлите нюанси са <Link to="/combinations/pastels" target="_blank"><i>пастелни</i></Link>, бледи, студени оттенъци на чистия цвят.</div>
+                            <canvas id="blackCanvas" width="270" height="40" onClick={modifyColor}></canvas>
+                            <div>Черното потъмнява цвета. Сенките са дълбоки, топли и по-интензивни оттенъци.</div>
+                            <canvas id="greyCanvas" width="270" height="40" onClick={modifyColor}></canvas>
+                            <div>Сивото обезцветява. Тоновете са приглушени и слабо наситени оттенъци.</div>
+                        </section>
+                    </section>
+
+                    <section className="explore__preview">
+                        <h5 className="explore__info">Преглед на цвета</h5>
+                        <div id="previewWitheText">Бял текст</div>
+                        <div id="previewTextOnWithe">Бял фон</div>
+                        <div id="previewBlackText">Черен текст</div>
+                        <div id="previewTextOnBlack">Черен фон</div>
+                    </section>
+
+                    <section className="explore__values">
+                        <h5 className="explore__info">Цветни стойности</h5>
+                        <li>{name ? name : 'няма име'}</li>
+                        <span>Браузърите поддържат 140 <Link target="_blank" to="/color-explore/color-names"><i>стандартни имена.</i></Link></span>
+                        <li>{rgb}</li>
+                        <span>RGB стойностите показват какво количество червено, зелено и синьо се съдържа в цвета.
+                            <br /> Всеки един от тройката цветове може да бъде от 0 до 255.</span>
+                        <li>#{hex}</li>
+                        <span>Нex кодът е шест-цифрено шестнадесетично число и определя интензитета на червеното, зеленото и синьото.</span>
+                        <li>hsl({hsl})</li>
+                        <span>HSL стойността се представя с параметрите цвят, наситеност и яркост.</span>
+                        <li>cmyk({cmyk})</li>
+                        <span>CMYK стойността дава количеството на четирите мастила, използвани в цветния печат: циан, маджента, жълто и черно.</span>
+                    </section>
+                </>
+            )}
         </section>
     );
 };
